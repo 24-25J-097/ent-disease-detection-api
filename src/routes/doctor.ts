43fd6da3a10f0d0c1e12bb895887a10cdcdda6ac
Prashantha K.G.M <it21169908@my.sliit.lk>
2025-03-18 23:43:09 +0530

    Code refactoring

--- src/routes/doctor.ts
+++ src/routes/doctor.ts
@@ -1,5 +1,5 @@
 import {Express} from 'express';
-import * as DiagnosisEp from "../end-points/Cholesteatoma.ep";
+import * as DiagnosisEp from "../end-points/CholesteatomaDiagnosis.ep";
 import * as SinusitisDiagnosisEp from "../end-points/SinusitisDiagnosis.ep";
 import {upload} from "../middleware/upload";
 import * as PharyngitisDiagnosisEp from "../end-points/PharyngitisDiagnosis.ep";
@@ -7,33 +7,38 @@ import * as PharyngitisDiagnosisEp from "../end-points/PharyngitisDiagnosis.ep";
 export function DoctorRoutesInit(app: Express) {
 
     /* PUBLIC ROUTES ===================================== */
+
+    // CHOLESTEATOMA ROUTES
     app.post('/api/public/diagnosis/cholesteatoma', upload.single("endoscopyImage"), DiagnosisEp.cholesteatomaDiagnosis);
     app.post('/api/public/diagnosis/cholesteatoma/accept', DiagnosisEp.cholesteatomaDiagnosisAccept);
     app.get('/api/public/diagnosis/cholesteatoma/image/:_id', DiagnosisEp.cholesteatomaImage);
 
-
+    // SINUSITIS ROUTES
     app.post('/api/public/diagnosis/sinusitis', upload.single("watersViewXrayImage"), SinusitisDiagnosisEp.sinusitisDiagnosis);
     app.post('/api/public/diagnosis/sinusitis/accept', SinusitisDiagnosisEp.sinusitisDiagnosisAccept);
     app.get('/api/public/diagnosis/sinusitis/image/:_id', SinusitisDiagnosisEp.sinusitisImage);
 
+    // PHARYNGITIS ROUTES
     app.post('/api/public/diagnosis/pharyngitis', upload.single("throatImage"), PharyngitisDiagnosisEp.pharyngitisDiagnosis);
     app.post('/api/public/diagnosis/pharyngitis/accept', PharyngitisDiagnosisEp.pharyngitisDiagnosisAccept);
     app.get('/api/public/diagnosis/pharyngitis/image/:_id', PharyngitisDiagnosisEp.pharyngitisImage);
 
 
     /* AUTH ROUTES ===================================== */
+
+    // CHOLESTEATOMA ROUTES
     app.post('/api/doctor/diagnosis/cholesteatoma', upload.single("endoscopyImage"), DiagnosisEp.cholesteatomaDiagnosis);
     app.post('/api/doctor/diagnosis/cholesteatoma/accept', DiagnosisEp.cholesteatomaDiagnosisAccept);
     app.get('/api/doctor/diagnosis/cholesteatoma/reports', DiagnosisEp.cholesteatomaReports);
     app.get('/api/doctor/diagnosis/cholesteatoma', DiagnosisEp.cholesteatoma);
 
-
+    // SINUSITIS ROUTES
     app.post('/api/doctor/diagnosis/sinusitis', upload.single("watersViewXrayImage"), SinusitisDiagnosisEp.sinusitisDiagnosis);
     app.post('/api/doctor/diagnosis/sinusitis/accept', SinusitisDiagnosisEp.sinusitisDiagnosisAccept);
     app.get('/api/doctor/diagnosis/sinusitis/reports', SinusitisDiagnosisEp.sinusitisReports);
     app.get('/api/doctor/diagnosis/sinusitis', SinusitisDiagnosisEp.sinusitis);
 
-
+    // PHARYNGITIS ROUTES
     app.post('/api/doctor/diagnosis/pharyngitis', upload.single("throatImage"), PharyngitisDiagnosisEp.pharyngitisDiagnosis);
     app.post('/api/doctor/diagnosis/pharyngitis/accept', PharyngitisDiagnosisEp.pharyngitisDiagnosisAccept);
     app.get('/api/doctor/diagnosis/pharyngitis/reports', PharyngitisDiagnosisEp.pharyngitisReports);
