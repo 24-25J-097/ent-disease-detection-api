4089dfb0f4801054f6ad1f55e15917123e9853be
Fonseka M.M.N.H <it21156410@my.sliit.lk>
2025-05-24 16:16:16 +0530

    [debug] add environment log

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,8 @@ import {AppLogger, ErrorLogger} from "./utils/logging";
 // Load environment variables
 const isProduction = process.env.NODE_ENV === "production";
 console.log("Environment: ", process.env.NODE_ENV)
+console.log("Allowed Origin: ", process.env.ALLOWED_ORIGIN)
+console.log("Is Production: ", isProduction)
 
 const port = env.PORT;
 
