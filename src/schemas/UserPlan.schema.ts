fd79a5743ec656f993be7bcc2a6309845fa92bc8
Fonseka M.M.N.H <it21156410@my.sliit.lk>
2025-05-26 01:21:30 +0530

    [commercialization] fix user ref

--- src/schemas/UserPlan.schema.ts
+++ src/schemas/UserPlan.schema.ts
@@ -4,7 +4,7 @@ import {IUserPlan} from "../models/UserPlan.model";
 export const UserPlanSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
+        ref: 'users',
         required: true
     },
     package: {
